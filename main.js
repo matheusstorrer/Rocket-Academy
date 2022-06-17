903edc90d8727c03d0b5c858fb14d8fa4fcd5c51
Matheus Storrer Marcal <66227432+matheusstorrer@users.noreply.github.com>
2022-06-16 21:11:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementsByClassName('nav-links__item-body')[3].innerText =
 // const contato = document.getElementsByClassName('topbar__item')[1]
 // document.getElementsByClassName('topbar__row')[0].removeChild(contato)
 
-const cadastro = document.getElementsByClassName('topbar__item')[1]
+const cadastro = document.getElementsByClassName('topbar__item')[2]
 document.getElementsByClassName('topbar__row')[0].removeChild(cadastro)
 
 const vertodos = document.getElementsByClassName('menu__item-link')[7]

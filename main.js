95c334c6ffc8f8c631a3ee661339017585da1046
Matheus Storrer Marcal <66227432+matheusstorrer@users.noreply.github.com>
2022-07-21 11:57:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ vertodos.innerText = 'Ver Todos'
 const endereco = document.getElementsByClassName('footer-contacts__contacts')[0].firstElementChild
 endereco.remove()
 
-const telefone = document.getElementsByClassName('row')[1].firstElementChild
-telefone.remove()
+// const telefone = document.getElementsByClassName('row')[1].firstElementChild
+// telefone.remove()
 
 const removemapasite = document.getElementsByClassName('site-footer__widgets')[0].children[0].children[1]
 removemapasite.remove()

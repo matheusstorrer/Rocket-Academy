457338936e4256a8364c236ce972204a37be27da
Matheus Storrer Marcal <66227432+matheusstorrer@users.noreply.github.com>
2023-01-31 01:50:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ vertodos.innerText = 'Ver Todos'
 const menumobile = document.getElementsByClassName('mobile-links__item')[0]
 menumobile.remove()
 
-const endereco = document.getElementsByClassName('footer-contacts__contacts')[0].firstElementChild
-endereco.remove()
+// const endereco = document.getElementsByClassName('footer-contacts__contacts')[0].firstElementChild
+// endereco.remove()
 
-const telefone = document.getElementsByClassName('site-footer__widgets')[0].children[0].children[0]
-telefone.remove()
+// const telefone = document.getElementsByClassName('site-footer__widgets')[0].children[0].children[0]
+// telefone.remove()
 
-const removemapasite = document.getElementsByClassName('site-footer__widgets')[0].children[0].children[0]
-removemapasite.remove()
+// const removemapasite = document.getElementsByClassName('site-footer__widgets')[0].children[0].children[0]
+// removemapasite.remove()
 
 const mercadopago = document.getElementsByClassName('mp')[0]
 mercadopago.src = 'https://matheusstorrer.github.io/Rocket-Academy/12xML.jpg'

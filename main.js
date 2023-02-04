b058a09e85148853394640a8e96b946f061871b2
Matheus Storrer Marcal <66227432+matheusstorrer@users.noreply.github.com>
2023-02-04 08:27:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -313,6 +313,7 @@ if (window.innerWidth < 768) {
   document.getElementsByClassName('block-slideshow__slide-image--mobile')[0].outerHTML = '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/Banner_MOBILE_CAD.png\')"></div>'
   document.getElementsByClassName('block-slideshow__slide-image--mobile')[1].outerHTML = '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/Banner_MOBILE_EXCEL.png\')"></div>'
   document.getElementsByClassName('block-slideshow__slide-image--mobile')[2].outerHTML = '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/Banner_MOBILE_CANVA.png\')"></div>'
+  document.getElementsByClassName('block-slideshow__slide-image--mobile')[3].outerHTML = '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/Banner%20ROCKET_MOBILE.png\')"></div>'
 
 }
 

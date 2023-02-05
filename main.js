61ba839049416ff90aae7d69dbac56a573253665
Matheus Storrer <pkw3@hotmail.com.br>
2023-02-05 20:51:17 -0300

    123#

--- main.js
+++ main.js
@@ -293,7 +293,7 @@ if (window.innerWidth < 768) {
   document.getElementsByClassName(
     'block-slideshow__slide-image--mobile'
   )[3].outerHTML =
-    '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/Banner%20ROCKET_MOBILE.png\')"></div>'
+    '<div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(\'https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/main/banners/Banner%20ROCKET_MOBILE_INSTITUCIONAL.png\')"></div>'
 
   // ------------------------------------------------------- //
 

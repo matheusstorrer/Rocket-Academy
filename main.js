179f808883334b6be055c153bf3a5b0d53a9b598
Matheus Storrer <pkw3@hotmail.com.br>
2023-02-06 13:17:10 -0300

    123

--- main.js
+++ main.js
@@ -12,15 +12,18 @@ topbarPortal.setAttribute('style', 'margin-right: 16px')
 
 const menumobile = document.getElementsByClassName('mobile-links__item')[0]
 menumobile.remove()
+if (
+  window.location.pathname === 'https://rocketacademyschool.com.br/index.html'
+) {
+  document.getElementsByClassName('category-card__image')[2].innerHTML =
+    '<a href="categoria/fotografia" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/cbe686a79d319d75302fb9c0571efe5b28213408/icons/camera.svg" alt="Cursos de Fotografia"></a>'
 
-document.getElementsByClassName('category-card__image')[2].innerHTML =
-  '<a href="categoria/fotografia" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/cbe686a79d319d75302fb9c0571efe5b28213408/icons/camera.svg" alt="Cursos de Fotografia"></a>'
+  document.getElementsByClassName('category-card__image')[1].innerHTML =
+    '<a href="categoria/youtuber" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/ab2bfa213a224bc2ed373200ae102f1ce40b9284/icons/youtube_destaque.svg" alt="Cursos de Youtuber"></a>'
 
-document.getElementsByClassName('category-card__image')[1].innerHTML =
-  '<a href="categoria/youtuber" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/ab2bfa213a224bc2ed373200ae102f1ce40b9284/icons/youtube_destaque.svg" alt="Cursos de Youtuber"></a>'
-
-document.getElementsByClassName('category-card__image')[0].innerHTML =
-  '<a href="categoria/especialista-em-excel" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/2c0cf23781d9feafbf37fabc6f803cd58fd0c85c/icons/excel-app.svg" alt="Cursos de Excel"></a>'
+  document.getElementsByClassName('category-card__image')[0].innerHTML =
+    '<a href="categoria/especialista-em-excel" class="destaques_icon keychainify-checked"><img src="https://raw.githubusercontent.com/matheusstorrer/Rocket-Academy/2c0cf23781d9feafbf37fabc6f803cd58fd0c85c/icons/excel-app.svg" alt="Cursos de Excel"></a>'
+}
 
 const removemapasite = document.getElementsByClassName('footer-links')[0]
 removemapasite.remove()
